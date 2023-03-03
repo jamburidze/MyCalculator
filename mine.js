226e51e138cb174fa8059b9ddc156796d0a5afe1
Giorgi <116874191+jamburidze@users.noreply.github.com>
2023-03-03 13:43:54 -0800

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,64 @@
+let shower = document.getElementById("shower");
+
+function fun1(){
+    shower.value += "1"
+}
+
+
+function fun2(){
+    shower.value += "2"
+}
+
+
+function fun3(){
+    shower.value += "3"
+}
+
+
+function fun4(){
+    shower.value += "+"
+}
+
+function fun5(){
+    shower.value += "4"
+}
+
+function fun6(){
+    shower.value += "5"
+}
+
+function fun7(){
+    shower.value += "6"
+}
+
+function fun8(){
+    shower.value += "-"
+}
+
+function fun9(){
+    shower.value += "7"
+}
+function fun10(){
+    shower.value += "8"
+}
+function fun11(){
+    shower.value += "9"
+}
+function fun12(){
+    shower.value += "*"
+}
+function fun13(){
+    shower.value += "/"
+}
+function fun14(){
+    shower.value += "0"
+}
+function fun15(){
+    shower.value = ""
+}
+function fun16(){
+    shower.value = eval (shower.value)
+}
+
+
+
